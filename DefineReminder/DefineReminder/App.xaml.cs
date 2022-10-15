a40ffae5e704a0f0b2f85ea210be5524f49f6d96
JaMe <jame_581@windowslive.com>
2022-10-15 13:51:36 +0200

    Add project files.

---
+++ DefineReminder/DefineReminder/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DefineReminder.Services;
+using DefineReminder.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DefineReminder
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DefineReminder/DefineReminder/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DefineReminder.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DefineReminder.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
