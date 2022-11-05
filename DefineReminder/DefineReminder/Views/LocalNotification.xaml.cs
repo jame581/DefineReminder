d67705374cd14b7d779a2e92f44e3b23423b66c2
JaMe <jame_581@windowslive.com>
2022-11-05 13:14:21 +0100

    Local notification for Android

---
+++ DefineReminder/DefineReminder/Views/LocalNotification.xaml.cs
@@ -0,0 +1,56 @@
+﻿using DefineReminder.Events;
+using DefineReminder.Services;
+using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DefineReminder.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LocalNotification : ContentPage
+    {
+        INotificationManager notificationManager;
+        int notificationNumber = 0;
+
+        public LocalNotification()
+        {
+            InitializeComponent();
+
+            notificationManager = DependencyService.Get<INotificationManager>();
+            notificationManager.NotificationReceived += (sender, eventArgs) =>
+            {
+                var evtData = (NotificationEventArgs)eventArgs;
+                ShowNotification(evtData.Title, evtData.Message);
+            };
+        }
+
+        void OnSendClick(object sender, EventArgs e)
+        {
+            notificationNumber++;
+            string title = $"Local Notification #{notificationNumber}";
+            string message = $"You have now received {notificationNumber} notifications!";
+            notificationManager.SendNotification(title, message);
+        }
+
+        void OnScheduleClick(object sender, EventArgs e)
+        {
+            notificationNumber++;
+            string title = $"Local Notification #{notificationNumber}";
+            string message = $"You have now received {notificationNumber} notifications!";
+            notificationManager.SendNotification(title, message, DateTime.Now.AddSeconds(10));
+        }
+
+        void ShowNotification(string title, string message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var msg = new Label()
+                {
+                    Text = $"Notification Received:\nTitle: {title}\nMessage: {message}"
+                };
+                stackLayout.Children.Add(msg);
+            });
+        }
+    }
+}
